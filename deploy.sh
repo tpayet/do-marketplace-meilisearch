8a4337a5157e9f90f159d8332f2c312ba0975dec
Samuel Jimenez <sjimenezre@gmail.com>
2020-04-16 14:15:14 +0200

    Create, enable and start meilisearch service

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,27 @@ export DEBIAN_FRONTEND=noninteractive
 echo "deb http://ftp.de.debian.org/debian sid main" >> /etc/apt/sources.list
 apt update -y
 apt install git curl gcc make nginx -y
-printf "\n" | apt install gcc-10 -y
+apt install gcc-10 -y
 
 # Install MeiliSearch v0.10.0
 wget --directory-prefix=/etc/meilisearch/ https://github.com/meilisearch/MeiliSearch/releases/download/v0.10.0/meilisearch.deb
 apt install /etc/meilisearch/meilisearch.deb
+
+# Prepare systemd service for MeiliSearch
+mv target/release/meilisearch /usr/bin/
+cat << EOF >/etc/systemd/system/meilisearch.service
+[Unit]
+Description=MeiliSearch
+After=systend-user-sessions.service
+
+[Service]
+Type=simple
+ExecStart=/usr/bin/meilisearch
+
+[Install]
+WantedBy=default.target
+EOF
+
+# Start MeiliSearch service
+systemctl enable meilisearch
+systemctl start meilisearch
